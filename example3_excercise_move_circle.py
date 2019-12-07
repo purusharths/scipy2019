35290961679a3a2444885ec97faab9f5c7c444f7
Purusharth S <purusharth.saxena1@gmail.com>
2019-12-07 13:37:41 +0530

    Updated example

--- example3_excercise_move_circle.py
+++ example3_excercise_move_circle.py
@@ -3,7 +3,8 @@ from manimlib.imports import *
 class MoveCircleExample(Scene):
     def construct(self):
         circle = Circle()
-        ###
-        ###
-        ##
+        square = Square()
+        ### Move circle to (3,3)
+        # transform circle to square
+        #  
 
